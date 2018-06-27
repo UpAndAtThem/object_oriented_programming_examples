702a80a3041b097a3e7b2f9f4c7fa2d20eca7fbe
Jason Nelson <Jasonn318@gmail.com>
2018-06-27 17:26:27 -0500

    added attr_accessor for name instead of explicit definitonscommented out other class for show they are the same class

--- basics_classes_and_objects/create_the_class.rb
+++ basics_classes_and_objects/create_the_class.rb
@@ -1,10 +1,26 @@
-class Cat
-  def initialize(name)
-    @name = name
-  end
+# class Cat
+#   def initialize(name = "unknown")
+#     @name = name
+#   end
 
-  def name
-    @name
+#   def name
+#     @name
+#   end
+
+#   def name=(name)
+#     @name = name
+#   end
+
+#   def greet
+#     "hello my name is #{name}!"
+#   end
+# end
+
+class Cat
+  attr_accessor :name
+
+  def initialize(name = "unknown")
+    @name = name
   end
 
   def greet
@@ -12,6 +28,4 @@ class Cat
   end
 end
 
-kitty = Cat.new("Sophie")
-
-p kitty.greet
+kitty = Cat.new()
