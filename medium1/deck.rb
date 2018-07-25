19dfba4dfa02253a451f4f2b35b482365c43c653
Jason Nelson <Jasonn318@gmail.com>
2018-07-25 18:56:00 -0500

    add deck.rb

---
+++ medium1/deck.rb
@@ -0,0 +1,71 @@
+# Using the Card class from the previous exercise, 
+# create a Deck class that contains all of the standard 52 playing cards. 
+# Use the following code to start your work:
+
+class Card
+  attr_reader :rank, :suit
+  include Comparable
+
+  FACE_ORDER = [2, 3, 4, 5, 6, 7, 8, 9 , 10, 'Jack', 'Queen', 'King', 'Ace'].zip((2..14)).to_h
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def ==(other_card)
+    rank == other_card.rank
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  protected
+
+  def <=>(other_card)
+    FACE_ORDER[rank] <=> FACE_ORDER[other_card.rank]
+  end
+end
+
+class Deck
+  RANKS = (2..10).to_a + %w(Jack Queen King Ace).freeze
+  SUITS = %w(Hearts Clubs Diamonds Spades).freeze
+
+  include Comparable
+
+  attr_reader :cards
+
+  def initialize
+    @cards = create_deck
+  end
+
+  def draw
+    cards.pop
+  end
+
+  def count
+    cards.size
+  end
+
+  protected
+
+  def <=>(other_deck)
+    cards <=> other_deck.cards.shuffle
+  end
+
+  def create_deck
+    cards = []
+
+    RANKS.each do |rank|
+      SUITS.each do |suit|
+        cards << Card.new(rank, suit)
+      end
+    end
+
+    cards
+  end
+end
+
+
+p Deck.new == Deck.new
