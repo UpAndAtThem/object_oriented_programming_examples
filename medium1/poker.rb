18b3448ac89fb0d29650281729e777ca10b8139b
Jason Nelson <Jasonn318@gmail.com>
2018-08-19 23:43:51 -0500

    add percentage instance variable

--- medium1/poker.rb
+++ medium1/poker.rb
@@ -184,46 +184,42 @@ class PokerHand
   end
 end
 
-# p PokerHand.occur_percentage("Royal flush")
-
 class Odds
-  def initialize(hand, occurances_needed)
-    @hand = hand
+  def initialize(hand_name, occurances_needed)
+    @hand_name = hand_name
     @occurances_needed = occurances_needed
     @iterations = 0
     @count = 0
     @total_occurances = 0
-    @percentage = nil
   end
 
   def percentage
     loop do
       loop do
-        binding.pry
-        @deck = Deck.new
-        @hand = PokerHand.new(@deck.deal) 
+        deck = Deck.new
+        @hand = PokerHand.new(deck.deal) 
         @count += 1
-        if [@hand].include? @hand.evaluate
-          count += 1
-          num_occur += 1
-          puts "-------------"
-          puts "TOTAL NUMBER TIMES #{@hand.upcase} DEALT: #{num_occur}"
-          puts "TOTAL NUMBER HANDS DEALT: #{count}"
-          puts "HAND OCCURANCE PERCENTAGE: %#{(num_occur/count.to_f) * 100}\n\n"
-          dealt_cards.print
-        end
-        break if num_occur == occurances_needed
+
+        @total_occurances += 1 if [@hand_name].include? @hand.evaluate
+
+        break if @total_occurances == @occurances_needed
       end
-      puts "\n\n1 out of #{count.to_f/num_occur} hands will be dealt a #{hand}\n\n"
-      result << count
-      iterations += 1
-      num_occur = 0
-      count = 0
-      break if iterations == 5
+
+      @percentage = (@total_occurances/@count.to_f) * 100
+
+      @total_occurances = 0
+      @count = 0
+
+      @iterations += 1
+      break if @iterations == 15
     end
+    @percentage
   end
 end
 
 deck = Deck.new.shuffle
 
-odds_instance = Odds.new(PokerHand.new(deck.deal), 10)
+odds_instance = Odds.new("Flush", 10)
+
+p odds_instance.percentage
+
